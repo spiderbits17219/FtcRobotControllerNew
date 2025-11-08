44bcf47a611a5de0ae3d5bf85dd0516ea8699862
coraseymour <carpediem.cora@gmail.com>
2025-11-08 10:01:35 -0500

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop2526.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop2526.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "Teleop2526")
@@ -22,6 +23,10 @@ public class Teleop2526 extends LinearOpMode {
         DcMotor transferMotor = hardwareMap.get(DcMotor.class, "transferMotor");
         DcMotor shooter1 = hardwareMap.get(DcMotor.class, "Shooter1");
         DcMotor shooter2 = hardwareMap.get(DcMotor.class, "Shooter2");
+
+        // --- New Servo for feeder ---
+        Servo feederServo = hardwareMap.get(Servo.class, "feederServo");
+
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.FORWARD);
@@ -31,6 +36,12 @@ public class Teleop2526 extends LinearOpMode {
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // --- Shooter Encoders ---
+        shooter1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        shooter2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        shooter1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooter2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         //Reset encoders for intake
         intakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -43,7 +54,9 @@ public class Teleop2526 extends LinearOpMode {
         // Wait for the game to start
         waitForStart();
         runtime.reset();
-
+// PID sync variables
+        double kP = 0.002; // proportional constant to sync shooter2
+        double shooterPower = 1.0;
         // Run until the end of the match
         while (opModeIsActive()) {
 
@@ -89,16 +102,36 @@ public class Teleop2526 extends LinearOpMode {
                 shooter1.setPower(0);
                 shooter2.setPower(0);
             }
+            // --- Shooter Control with Encoder Sync ---
+            if (gamepad2.right_bumper) {
+                shooter1.setPower(shooterPower);
+                // keep shooter2 in sync using encoder feedback
+                int pos1 = shooter1.getCurrentPosition();
+                int pos2 = shooter2.getCurrentPosition();
+                double error = pos1 - pos2;
+                double syncAdjust = kP * error;
+                shooter2.setPower(shooterPower + syncAdjust);
+                // --- Servo Control based on shooter encoder ---
+                int SHOOTER_READY_POSITION = 500; // <-- replace with your desired encoder target
+                if (Math.abs(pos1) >= SHOOTER_READY_POSITION) {
+                    feederServo.setPosition(1.0); // open servo
+                } else {
+                    feederServo.setPosition(0.0); // keep closed until ready
+                }
+            } else {
+                shooter1.setPower(0);
+                shooter2.setPower(0);
+                feederServo.setPosition(0.0);
+            }
 
             // --- Telemetry ---
+            // --- Telemetry ---
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Drive Power",
-                    "FL: %.2f | FR: %.2f | BL: %.2f | BR: %.2f",
-                    frontLeft.getPower(), frontRight.getPower(),
-                    backLeft.getPower(), backRight.getPower());
-            telemetry.addData("Intake", intakeMotor.getPower());
-            telemetry.addData("Shooter1", shooter1.getPower());
-            telemetry.addData("Shooter2", shooter2.getPower());
+            telemetry.addData("Shooter1 Pos", shooter1.getCurrentPosition());
+            telemetry.addData("Shooter2 Pos", shooter2.getCurrentPosition());
+            telemetry.addData("Shooter Error", shooter1.getCurrentPosition() - shooter2.getCurrentPosition());
+            telemetry.addData("Shooter2 Adjusted Power", shooter2.getPower());
+            telemetry.addData("Feeder Servo", feederServo.getPosition());
             telemetry.update();
         }
     }
