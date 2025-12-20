e7d8e9e11ca7f91169e987f76b49710c656df647
nehaparepally <neha.parepally@gmail.com>
2025-12-20 16:10:21 -0500

    teleop with new ftc lib pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop2526.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop2526.java
@@ -1,32 +1,52 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.CRServo;
+
+
 
 @Config
 @TeleOp(name = "Teleop2526", group = "TeleOp")
 public class Teleop2526 extends LinearOpMode {
+    // --- FTC Dashboard tunable variables ---
+    public static double desiredVelocity = 800;
+    public static double kP1 = 4;
+    public static double kP2 = 8;
 
-    // FTC Dashboard tunable variables
-    public static double desiredVelocity = 0.001;
-    public static double kP1 = 0.002;
-    public static double kP2 = 0.002;
-    public static double shooterPower1 = 0.8;
-    public static double shooterPower2 = -0.8;
+    public static double  kI1 = 4;
 
+    public static double kI2 = 4;
+
+    public static double kD1 = 0.05;
+
+    public static double kD2 = 0.05;
+
+    public static double kF1 = 0;
+
+    public static double kF2 = 0;
+
+
+
+
+    public static double shooterVelocity1 = 0;
+    public static double shooterVelocity2 = 0;
     private final ElapsedTime runtime = new ElapsedTime();
+
+    // Creates a PIDFController with gains kP, kI, kD, and kF
+    PIDFController pidf1 = new PIDFController(kP1, kI1, kD1, kF1);
+    PIDFController pidf2 = new PIDFController(kP2, kI2, kD2, kF2);
 
     @Override
     public void runOpMode() {
-
         // Initialize hardware
         DcMotor frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         DcMotor frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
@@ -34,268 +54,148 @@ public class Teleop2526 extends LinearOpMode {
         DcMotor backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         DcMotor intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
         DcMotor transferMotor = hardwareMap.get(DcMotor.class, "transferMotor");
-        DcMotor shooter1 = hardwareMap.get(DcMotor.class, "Shooter1");
-        DcMotor shooter2 = hardwareMap.get(DcMotor.class, "Shooter2");
-
-        Servo feederServo = hardwareMap.get(Servo.class, "feederServo");
-        Servo blockerServo = hardwareMap.get(Servo.class, "blockerServo");
-        CRServo liftServo = hardwareMap.get(CRServo.class, "cr_servo_name");
+        MotorEx shooter1 = new MotorEx(hardwareMap, "shooter1", Motor.GoBILDA.RPM_312);
+        MotorEx shooter2 = new MotorEx(hardwareMap, "shooter2", Motor.GoBILDA.RPM_312);
+        Servo shooterServo = hardwareMap.get(Servo.class, "shooterServo");
+        Servo intakeServo = hardwareMap.get(Servo.class, "intakeServo");
+        CRServo liftServo = hardwareMap.get(CRServo.class, "liftServo");
         Servo holdServo1 = hardwareMap.get(Servo.class, "holdServo1");
         Servo holdServo2 = hardwareMap.get(Servo.class, "holdServo2");
+        holdServo1.setPosition(0.5);
+        holdServo2.setPosition(0);
+        liftServo.setPower(0);
+        shooterServo.setPosition(0.6);
+        shooter1.setRunMode(Motor.RunMode.VelocityControl);
+        shooter2.setRunMode(Motor.RunMode.VelocityControl);
+//
+//        double[] coeffs1 = shooter1.getVeloCoefficients();
+//        kP1 = coeffs1[0];
+//        kI1 = coeffs1[1];
+//        kD1 = coeffs1[2];
+//
+//        double[] coeffs2 = shooter2.getVeloCoefficients();
+//        kP2 = coeffs2[0];
+//        kI2 = coeffs2[1];
+//        kD2 = coeffs2[2];
 
-//        Servo feederServo = hardwareMap.get(Servo.class, "feederServo");
-//        Servo blockerServo = hardwareMap.get(Servo.class, "blockerServo");
-//        Servo liftServo = (Servo) hardwareMap.get(CRServo.class, "liftServo");
-//        Servo holdServo1 = hardwareMap.get(Servo.class, "holdServo1");
-//        Servo holdServo2 = hardwareMap.get(Servo.class, "holdServo2");
-
-
-        // Motor directions
+        boolean shooterServoOpen = false;
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.REVERSE);
-
+        shooter1.motor.setDirection(DcMotor.Direction.FORWARD);
+        shooter2.motor.setDirection(DcMotor.Direction.REVERSE);
         // Brake behavior
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        // Shooter encoders
-        shooter1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        shooter2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        shooter1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        shooter2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
         // Intake encoder reset
         intakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-
+        // --- FTC Dashboard Setup ---
         // --- FTC Dashboard Setup ---
         FtcDashboard dashboard = FtcDashboard.getInstance();
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
-
         waitForStart();
         runtime.reset();
-
-        double lastPos1 = shooter1.getCurrentPosition();
-        double lastPos2 = shooter2.getCurrentPosition();
+        double lastPos1 = shooter1.motor.getCurrentPosition();
+        double lastPos2 = shooter2.motor.getCurrentPosition();
         double lastTime = runtime.time();
-
         while (opModeIsActive()) {
-
             // --- Drive control ---
-            double leftDrive = -gamepad1.left_stick_y;
-            double rightDrive = -gamepad1.right_stick_y;
-
-            holdServo1.setPosition(0.5);
-            holdServo2.setPosition(-0.5);
-            liftServo.setPower(1);
-
+            double leftDrive = gamepad1.left_stick_y;
+            double rightDrive = gamepad1.right_stick_y;
+            if (gamepad1.y) {
+                sleep(1000);
+            }
             frontLeft.setPower(leftDrive);
             frontRight.setPower(rightDrive);
             backLeft.setPower(leftDrive);
             backRight.setPower(rightDrive);
-
             // --- Intake ---
-
-
             if (gamepad2.a) {
-                intakeMotor.setPower(1.0);
+                intakeMotor.setPower(0.9);
             } else if (gamepad2.y) {
-                intakeMotor.setPower(-1.0);
+                intakeMotor.setPower(-0.9);
             } else {
                 intakeMotor.setPower(0);
+            }
+            if (gamepad2.dpad_left) {
+                transferMotor.setPower(0.5);
+                transferMotor.setPower(-0.8);
             }
 
             // --- Transfer ---
             if (gamepad2.dpad_down) {
-                transferMotor.setPower(1.0);
+                transferMotor.setPower(0.7);
             } else if (gamepad2.dpad_up) {
-                transferMotor.setPower(-1.0);
+                transferMotor.setPower(-0.7);
             } else {
                 transferMotor.setPower(0);
             }
 
-
-            if (gamepad2.left_bumper) {
-                liftServo.setPower(1);
-                sleep(1000);
+            if (gamepad1.left_bumper && gamepad1.right_bumper) {
+                liftServo.setPower(-1);
+                sleep(1950);
                 liftServo.setPower(0);
                 holdServo1.setPosition(0);
-                holdServo2.setPosition(0);
+                holdServo2.setPosition(0.5);
                 sleep(1000);
                 liftServo.setPower(1);
-
+                sleep(1200);
+                liftServo.setPower(0);
             }
-//
-            if (gamepad2.right_stick_button){
-                blockerServo.setPosition(0);
+
+
+            if (gamepad2.b){
+                intakeServo.setPosition(0.7);
             } else {
-                blockerServo.setPosition(0.5);
+                intakeServo.setPosition(1);
             }
-//
 
-            // --- Lift servo ---
-//            if (gamepad2.right_bumper) {
-//                shooter1.setPower(1);
-//                shooter2.setPower(1);
+            if (gamepad2.left_bumper){
+                shooterServo.setPosition(1);
 
-//            if (gamepad2.left_bumper) {
-//                liftServo.setPosition(1);
-//                holdServo1.setPosition(1);
-//                holdServo2.setPosition(1);
-//                liftServo.setPosition(1);
-
-//            }
-//
-//            if (gamepad2.right_stick_button){
-//                blockerServo.setPosition(0.5);
-//            } else {
-//                blockerServo.setPosition(0);
-//            }
-//
-
-            // --- Lift servo ---
-            if (gamepad2.right_bumper) {
-                shooter1.setPower(1);
-                shooter2.setPower(1);
             } else {
-                shooter1.setPower(0);
-                shooter2.setPower(0);
+                shooterServo.setPosition(0.6);
+
             }
 
-            //  --- Shooter control with synchronization ---
-//            if (gamepad2.right_bumper) {
-//                int pos1 = shooter1.getCurrentPosition();
-//                int pos2 = shooter2.getCurrentPosition();
-//                double timeNow = runtime.time();
+//            double output1 = pidf1.calculate(
+//                    shooter1.getVelocity(),desiredVelocity
+//            );
 //
-//                double velocity1 = (pos1 - lastPos1) / (timeNow - lastTime);
-//                double velocity2 = (pos2 - lastPos2) / (timeNow - lastTime);
+//            double output2 = pidf2.calculate(
+//                    shooter2.getVelocity(),desiredVelocity
+//            );
 //
-//                double error1 = desiredVelocity - velocity1;
-//                double error2 = desiredVelocity - velocity2;
-//
-//                double syncAdjust1 = kP1 * error1;
-//                double syncAdjust2 = kP2 * error2;
-//
-//                shooter1.setPower(shooterPower1 + syncAdjust1);
-//                shooter2.setPower(shooterPower2 + syncAdjust2);
-//
-//                lastPos1 = pos1;
-//                lastPos2 = pos2;
-//                lastTime = timeNow;
+//            shooter1.set(output1);
+//            shooter2.set(output2);
+            shooter1.setVeloCoefficients(kP1, kI1, kD1);
+            shooter2.setVeloCoefficients(kP2, kI2, kD2);
 
-//            } else {
-//                shooter1.setPower(0);
-//                shooter2.setPower(0);
-//            }
+                shooter1.setVelocity(desiredVelocity);
+                shooter2.setVelocity(desiredVelocity);
 
-              //--- Shooter control with synchronization ---
-            if (gamepad2.right_bumper) {
-                int pos1 = shooter1.getCurrentPosition();
-                int pos2 = shooter2.getCurrentPosition();
-                double timeNow = runtime.time();
-
-                double velocity1 = (pos1 - lastPos1) / (timeNow - lastTime);
-                double velocity2 = (pos2 - lastPos2) / (timeNow - lastTime);
-
-                double error1 = desiredVelocity - velocity1;
-                double error2 = desiredVelocity - velocity2;
-
-                double syncAdjust1 = kP1 * error1;
-                double syncAdjust2 = kP2 * error2;
-
-                shooter1.setPower(shooterPower1 + syncAdjust1);
-                shooter2.setPower(shooterPower2 + syncAdjust2);
-
-                lastPos1 = pos1;
-                lastPos2 = pos2;
-                lastTime = timeNow;
-            } else {
-                shooter1.setPower(0);
-                shooter2.setPower(0);
-            }
 
             // --- Telemetry + Dashboard Data ---
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Shooter1 Pos", shooter1.getCurrentPosition());
-            telemetry.addData("Shooter2 Pos", shooter2.getCurrentPosition());
-            telemetry.addData("Shooter Error", shooter1.getCurrentPosition() - shooter2.getCurrentPosition());
-            telemetry.addData("Shooter1 Power", shooter1.getPower());
-            telemetry.addData("Shooter2 Power", shooter2.getPower());
-            telemetry.addData("Desired Velocity", desiredVelocity);
-//            telemetry.addData("Lift Servo", liftServo.getPosition());
+            telemetry.addData("shooter1.motor Pos", shooter1.motor.getCurrentPosition());
+            telemetry.addData("shooter2.motor Pos", shooter2.motor.getCurrentPosition());
+            telemetry.addData("Shooter(1-2) Delta", shooter1.motor.getCurrentPosition() - shooter2.motor.getCurrentPosition());
+            telemetry.addData("shooter1.motor Power", shooter1.motor.getPower());
+            telemetry.addData("shooter2.motor Power", shooter2.motor.getPower());
+            telemetry.addData("Left Flywheel Velocity", shooter1.getVelocity());
+            telemetry.addData("Right Flywheel Velocity", shooter2.getVelocity());
+            telemetry.addData("desired velocity", desiredVelocity);
+
+
+
             telemetry.update();
         }
-        }
     }
+}
 
-//
-//            if (gamepad2.left_bumper) {
-//                liftServo.setPosition(1);
-//                holdServo1.setPosition(1);
-//                holdServo2.setPosition(1);
-//                liftServo.setPosition(1);
-
-//            }
-//
-//            if (gamepad2.right_stick_button){
-//                blockerServo.setPosition(0.5);
-//            } else {
-//                blockerServo.setPosition(0);
-//            }
-//
-/*
-            // --- Lift servo ---
-            if (gamepad2.right_bumper) {
-                shooter1.setPower(1);
-                shooter2.setPower(1);
-            } else {
-                shooter1.setPower(0);
-                shooter2.setPower(0);
-            }
-*/
-            //  --- Shooter control with synchronization ---
-//            if (gamepad2.right_bumper) {
-//                int pos1 = shooter1.getCurrentPosition();
-//                int pos2 = shooter2.getCurrentPosition();
-//                double timeNow = runtime.time();
-//
-//                double velocity1 = (pos1 - lastPos1) / (timeNow - lastTime);
-//                double velocity2 = (pos2 - lastPos2) / (timeNow - lastTime);
-//
-//                double error1 = desiredVelocity - velocity1;
-//                double error2 = desiredVelocity - velocity2;
-//
-//                double syncAdjust1 = kP1 * error1;
-//                double syncAdjust2 = kP2 * error2;
-//
-//                shooter1.setPower(shooterPower1 + syncAdjust1);
-//                shooter2.setPower(shooterPower2 + syncAdjust2);
-//
-//                lastPos1 = pos1;
-//                lastPos2 = pos2;
-//                lastTime = timeNow;
-//            } else {
-//                shooter1.setPower(0);
-//                shooter2.setPower(0);
-//            }
-//
-
-//            // --- Telemetry + Dashboard Data ---
-//            telemetry.addData("Status", "Run Time: " + runtime.toString());
-//            telemetry.addData("Shooter1 Pos", shooter1.getCurrentPosition());
-//            telemetry.addData("Shooter2 Pos", shooter2.getCurrentPosition());
-//            telemetry.addData("Shooter Error", shooter1.getCurrentPosition() - shooter2.getCurrentPosition());
-//            telemetry.addData("Shooter1 Power", shooter1.getPower());
-//            telemetry.addData("Shooter2 Power", shooter2.getPower());
-//            telemetry.addData("Desired Velocity", desiredVelocity);
-////            telemetry.addData("Lift Servo", liftServo.getPosition());
-//            telemetry.update();
-//        }
